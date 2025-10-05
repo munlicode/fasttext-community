c74410b0d82fe74b607ab7cab796d358f63c6320
munlicode <nurzhanmuratkhan@gmail.com>
2025-10-05 16:54:03 +0500

    1. Updated docs 2. Fixed inconsistency with old cmd. 3. Updated project dependecies

--- download_model.py
+++ download_model.py
@@ -22,27 +22,39 @@ args = None
 
 def command_download(lang_id, if_exists):
     """
-        Download pre-trained common-crawl vectors from fastText's website
-        https://fasttext.cc/docs/en/crawl-vectors.html
+    Download pre-trained common-crawl vectors from fastText's website
+    https://fasttext.cc/docs/en/crawl-vectors.html
     """
-    fasttext.util.download_model(lang_id, if_exists)
+    fasttext.util.download_model(  # FIXME: Hardcoded values to keep function working. Needs update of command_download usage and its documentation
+        "vectors_crawl",
+        model_type="bin",
+        lang_id=lang_id,
+        if_exists=if_exists,
+    )
 
 
 def main():
     global args
 
     parser = argparse.ArgumentParser(
-        description='fastText helper tool to reduce model dimensions.')
-    parser.add_argument("language", type=str, default="en",
-                        help="language identifier of the pre-trained vectors. For example `en` or `fr`.")
-    parser.add_argument("--overwrite", action="store_true",
-                        help="overwrite if file exists.")
+        description="fastText helper tool to reduce model dimensions."
+    )
+    parser.add_argument(
+        "language",
+        type=str,
+        default="en",
+        help="language identifier of the pre-trained vectors. For example `en` or `fr`.",
+    )
+    parser.add_argument(
+        "--overwrite", action="store_true", help="overwrite if file exists."
+    )
 
     args = parser.parse_args()
 
-    command_download(args.language, if_exists=(
-        'overwrite' if args.overwrite else 'strict'))
+    command_download(
+        args.language, if_exists=("overwrite" if args.overwrite else "strict")
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
